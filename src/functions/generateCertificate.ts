29e364cc68cd8394d143b4a2cfbb992adbfca264
Renan Skonicezny Vilela <renansko@gmail.com>
2022-02-17 12:22:18 -0300

    Chapter VII

--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -6,6 +6,7 @@ import dayjs from 'dayjs'
 import { join } from 'path'
 import { readFileSync } from 'fs'
 import chromium from 'chrome-aws-lambda'
+import { S3 } from 'aws-sdk'
 
 
 interface ICreateCertificate {
@@ -33,16 +34,6 @@ const compileTemplate = async (data: ITemplate) => {
 export const handler: APIGatewayProxyHandler = async (event) => {
     const { id, name, grade } = JSON.parse(event.body) as ICreateCertificate
 
-    await document.put({
-        TableName: "user_certificate",
-        Item: {
-            id,
-            name,
-            grade,
-            created_at: new Date().getTime()
-        }
-    }).promise();
-
     const response = await document.query({
         TableName: "user_certificate",
         KeyConditionExpression: "id = :id",
@@ -50,6 +41,20 @@ export const handler: APIGatewayProxyHandler = async (event) => {
             ":id": id
         }
     }).promise();
+
+    const userAlreadyExist = response.Items[0];
+
+    if (!userAlreadyExist) {
+        await document.put({
+            TableName: "user_certificate",
+            Item: {
+                id,
+                name,
+                grade,
+                created_at: new Date().getTime()
+            }
+        }).promise();
+    }
 
     const medalPath = join(process.cwd(), "src", "templates", "selo.png");
     const medal = readFileSync(medalPath, "base64");
@@ -84,8 +89,27 @@ export const handler: APIGatewayProxyHandler = async (event) => {
 
     await browser.close();
 
+    const s3 = new S3();
+
+    await s3
+        .createBucket({
+            Bucket: "certificatenoderenan"
+        })
+        .promise()
+
+    await s3.putObject({
+        Bucket: "certificatenoderenan",
+        Key: `${id}.pdf`,
+        ACL: "public-read",
+        Body: pdf,
+        ContentType: "application/pdf"
+    }).promise();
+
     return {
         statusCode: 201,
-        body: JSON.stringify(response.Items[0]),
+        body: JSON.stringify({
+            message: "Certificado criado com sucesso!",
+            url: `https://certificatenoderenan.s3.amazonaws.com/${id}.pdf`
+        }),
     };
 };
